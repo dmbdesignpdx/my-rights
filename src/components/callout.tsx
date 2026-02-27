6d48273d585c1e98d5fb00b86174ad3f4bbfd8c2
Daniel Blake <daniel@danielblake.dev>
2026-02-26 18:14:48 -0800

    feat: add resource section (#18)
    - add a custom normalize plugin
    - update styling for callout
    - update content

--- src/components/callout.tsx
+++ src/components/callout.tsx
@@ -3,30 +3,61 @@ import { type PropsWithChildren } from "react";
 import { PiLightbulb, PiWarning } from "react-icons/pi";
 
 // Ours
-import { Alert as ShadcnAlert, AlertDescription } from "@/components/ui/alert";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert";
 import { cn } from "@/lib/utils";
 
 
+type CalloutType = "info" | "warning" | "text";
+
 export interface CalloutProps extends PropsWithChildren {
   text: string;
-  type: "info" | "warning";
+  type: CalloutType;
+  title?: string;
 }
 
 
-export function Callout({ type, text }: CalloutProps) {
-  const info = type === "info";
+const styles: Record<CalloutType, string> = {
+  info: "border-blue-200 bg-blue-50 text-blue-800 dark:border-blue-800 dark:bg-blue-950 dark:text-blue-200",
+  warning: "border-amber-200 bg-amber-50 text-amber-800 dark:border-amber-800 dark:bg-amber-950 dark:text-amber-200",
+  text: "border-purple-200 bg-purple-50 text-purple-800 dark:border-purple-800 dark:bg-purple-950 dark:text-purple-200",
+};
 
+const icon: Record<CalloutType, React.ReactNode> = {
+  info: <PiLightbulb aria-hidden="true" />,
+  warning: <PiWarning aria-hidden="true" />,
+  text: null,
+};
+
+export function Callout({
+  type,
+  text,
+  title,
+}: CalloutProps) {
   return (
-    <ShadcnAlert
-      className={cn(
-        info ? "bg-blue-50 text-blue-700" : "bg-amber-50 text-amber-700",
-        "tracking-wide",
+    <Alert className={cn(styles[type], "tracking-wide")} >
+      {icon[type]}
+      {title && (
+        <AlertTitle className="
+          text-zinc-800
+          dark:text-zinc-200
+        ">
+          {title}
+        </AlertTitle>
       )}
-    >
-      {info ? <PiLightbulb aria-hidden="true" /> : <PiWarning aria-hidden="true" />}
-      <AlertDescription className="text-inherit">
+      <AlertDescription className={cn(
+        type === "text" && `
+          text-lg
+          before:[content:open-quote]
+          after:[content:close-quote]
+        `,
+        "text-inherit",
+      )}>
         {text}
       </AlertDescription>
-    </ShadcnAlert>
+    </Alert>
   );
 }
